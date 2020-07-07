b564e5c8b9e56d2a67a414f95633e49ab647c281
Matt-A-Bennett <matthew.bennett@uclouvain.be>
2020-07-07 16:49:37 +0200

    ml practice k_nearest completed

--- ml_practice/ml.py
+++ ml_practice/ml.py
@@ -15,20 +15,20 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] *100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] *100.0
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume', ]]
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forcast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forcast_out = int(math.ceil(0.01*len(df)))
-print(forcast_out)
+forcast_out = int(math.ceil(0.1*len(df)))
+# print(forcast_out)
 
 df['label'] = df[forcast_col].shift(-forcast_out)
 
 X = np.array(df.drop(['label'],1))
 X = preprocessing.scale(X)
-X = X[:-forcast_out]
 X_lately = X[-forcast_out:]
+X = X[:-forcast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
@@ -49,7 +49,7 @@ accuracy = clf.score(X_test, y_test)
 
 forcast_set = clf.predict(X_lately)
 
-print(forcast_set, accuracy, forcast_out)
+# print(forcast_set, accuracy, forcast_out)
 
 df['Forecast'] = np.nan
 
