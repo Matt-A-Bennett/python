74d6a3c87e9bbe9f619f112b6f93612b27ac4ce8
Matthew Bennett <matthew.bennett@uclouvain.be>
2020-08-24 22:38:11 +0200

    added leaning dir and a plan

---
+++ learning/purpose.md
@@ -0,0 +1,1 @@
+try to make a set of classes and methods to implememnt linear algebra operations
--- ml_practice/svm.py
+++ ml_practice/svm.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing, neighbors
+from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -30,7 +30,7 @@ X = np.array(df.drop(["class"], 1))
 y = np.array(df["class"])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-clf = neighbors.KNeighborsClassifier()
+clf = svm.SVC(kernel='linear')
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
